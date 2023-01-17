8d6f88e366c8f6ef1d222de42f4b7f820f21ed26
Nicole Tuller <nicenikkituller@gmail.com>
2023-01-17 10:19:54 -0500

    All vendordeps + gradle changes, reframing of subsystems (rudimentary), subsystem constants framework

---
+++ src/main/java/frc/team832/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,11 @@
+package frc.team832.robot.subsystems;
+
+public class ArmSubsystem {
+    /**looking to implement some sort of arm in place.
+     * Depending on its shape and number of motors, it
+     * likely will involve some inverse kinematics calculations. 
+     * Some sort of quadrature encoder could also be put 
+     * in place to calculate angles + direction.
+    */
+
+}
---
+++ src/main/java/frc/team832/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,5 @@
+package frc.team832.robot.subsystems;
+
+public class VisionSubsystem {
+    //implement AprilTag vision system somehow
+}
