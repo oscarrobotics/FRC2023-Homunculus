e5c5257b54a0c3f16542095174c2e5da136945fc
Nicole Tuller <nicenikkituller@gmail.com>
2023-01-17 09:48:15 -0500

    Revert "Merge branch 'master' into 2023"
    This reverts commit 1e50f1797ccb09bafe2ae3f9c56a8850776c9e89, reversing
    changes made to eef8a5fbe729a8b21d377b41233cb9c3b7d4bef5.

--- src/main/java/frc/team832/robot/subsystems/IntakeSubsystem.java
+++
@@ -1,5 +0,0 @@
-package frc.team832.robot.subsystems;
-
-public class IntakeSubsystem {
-    
-}
--- src/main/java/frc/team832/robot/subsystems/VisionSubsystem.java
+++
@@ -1,5 +0,0 @@
-package frc.team832.robot.subsystems;
-
-public class VisionSubsystem {
-    //implement AprilTag vision system somehow
-}
