081f3432357f86993031d3054fc4ddb5affc656d
Ian Charboneau <ianchar13oneau@gmail.com>
2023-03-09 11:51:19 -0500

    Fixed maxoupt seters to use values from 0-1  instead of -1-0 and 0-1 for min and max respectivly, now all power  setting are positive

---
+++ src/main/java/frc/robot/TargetSelector.java
@@ -0,0 +1,8 @@
+package frc.robot;
+import io.github.oblarg.oblog.Loggable;
+import io.github.oblarg.oblog.annotations.Config;
+import io.github.oblarg.oblog.annotations.Log;
+
+public class TargetSelector implements Loggable {
+  
+}
