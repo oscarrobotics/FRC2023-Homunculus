80a1baa1f5a494ec494d5861064a3b71a897d196
Ian Charboneau <ianchar13oneau@gmail.com>
2023-03-09 17:53:26 -0500

    Begin defining Target poses for each scoring location

---
+++ src/main/java/frc/robot/TargetMap.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class TargetMap {
+
+  Pose2d[] redTargetPoses = new Pose2d[9];
+  Pose2d[] blueTargetPoses = new Pose2d[9];
+  Translation2d[] armTargets= new Translation2d[3];
+
+
+  TargetMap() {
+    
+  }
+  
+}
