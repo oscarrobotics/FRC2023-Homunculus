a795dabde81650ef54af229448ac5f4e16fefc76
Ian <ianchar13oneau@gmail.com>
2023-03-09 22:25:11 -0500

    create posemap of grid targest and substation targets may be close needs testing

--- src/main/java/frc/robot/TargetMap.java
+++ src/main/java/frc/robot/TargetMap.java
@@ -1,17 +1,73 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class TargetMap {
 
-  Pose2d[] redTargetPoses = new Pose2d[9];
   Pose2d[] blueTargetPoses = new Pose2d[9];
-  Translation2d[] armTargets= new Translation2d[3];
+  Pose2d[] redTargetPoses = new Pose2d[9];
+  Translation2d[] coneArmTargets= new Translation2d[3];
+  Translation2d[] cubeArmTargets= new Translation2d[3];
+
+  Pose2d[] blueStationPose = new Pose2d[4];
+  Pose2d[] redStationPose = new Pose2d[4];
+  Translation2d[] stationArmTargets = new Translation2d[4];
 
 
   TargetMap() {
-    
+      //collum 1 is bottom of field
+      blueTargetPoses[0] = new Pose2d(new Translation2d(2, 0.52), new Rotation2d(Math.toRadians(180)));
+      blueTargetPoses[1] = new Pose2d(new Translation2d(2, 1.06), new Rotation2d(Math.toRadians(180)));
+      blueTargetPoses[2] = new Pose2d(new Translation2d(2, 1.62), new Rotation2d(Math.toRadians(180)));
+      blueTargetPoses[3] = new Pose2d(new Translation2d(2, 2.19), new Rotation2d(Math.toRadians(180)));
+      blueTargetPoses[4] = new Pose2d(new Translation2d(2, 2.75), new Rotation2d(Math.toRadians(180)));
+      blueTargetPoses[5] = new Pose2d(new Translation2d(2, 3.31), new Rotation2d(Math.toRadians(180)));
+      blueTargetPoses[6] = new Pose2d(new Translation2d(2, 3.87), new Rotation2d(Math.toRadians(180)));
+      blueTargetPoses[7] = new Pose2d(new Translation2d(2, 4.43), new Rotation2d(Math.toRadians(180)));  
+      blueTargetPoses[8] = new Pose2d(new Translation2d(2, 4.98), new Rotation2d(Math.toRadians(180)));
+
+      redTargetPoses[0] = new Pose2d(new Translation2d(14.54, 0.52), new Rotation2d(Math.toRadians(0)));
+      redTargetPoses[1] = new Pose2d(new Translation2d(14.54, 1.06), new Rotation2d(Math.toRadians(0)));
+      redTargetPoses[2] = new Pose2d(new Translation2d(14.54, 1.62), new Rotation2d(Math.toRadians(0)));
+      redTargetPoses[3] = new Pose2d(new Translation2d(14.54, 2.19), new Rotation2d(Math.toRadians(0)));
+      redTargetPoses[4] = new Pose2d(new Translation2d(14.54, 2.75), new Rotation2d(Math.toRadians(0)));
+      redTargetPoses[5] = new Pose2d(new Translation2d(14.54, 3.31), new Rotation2d(Math.toRadians(0)));
+      redTargetPoses[6] = new Pose2d(new Translation2d(14.54, 3.87), new Rotation2d(Math.toRadians(0)));
+      redTargetPoses[7] = new Pose2d(new Translation2d(14.54, 4.43), new Rotation2d(Math.toRadians(0)));
+      redTargetPoses[8] = new Pose2d(new Translation2d(14.54, 4.98), new Rotation2d(Math.toRadians(0)));
+
+      coneArmTargets[0] = new Translation2d(0.25, 0.25); //0
+      coneArmTargets[1] = new Translation2d(0.61, 97); //87 4inchs higher
+      coneArmTargets[2] = new Translation2d(1.01, 0.127); //117 4inchs higher
+
+      cubeArmTargets[0] = new Translation2d(0.25, 0.25); //0
+      cubeArmTargets[1] = new Translation2d(0.61, 75); //60 6inchs higher
+      cubeArmTargets[2] = new Translation2d(1.01, 0.105); //90 6inchs higher
+
+      //charging stations
+      //top to bottom third is ground sigle sitation 4th is lined up with single station
+
+      redStationPose[0] = new Pose2d(new Translation2d(1.50, 7.29), new Rotation2d(Math.toRadians(171)));
+      redStationPose[1] = new Pose2d(new Translation2d(1.50, 6.25), new Rotation2d(Math.toRadians(191)));
+      redStationPose[2] = new Pose2d(new Translation2d(3.6, 7.2), new Rotation2d(Math.toRadians(165)));
+      redStationPose[3] = new Pose2d(new Translation2d(2.70, 6.90), new Rotation2d(Math.toRadians(98)));
+
+      blueStationPose[0] = new Pose2d(new Translation2d(15, 7.29), new Rotation2d(Math.toRadians(9.0)));
+      blueStationPose[1] = new Pose2d(new Translation2d(15, 6.25), new Rotation2d(Math.toRadians(345)));
+      blueStationPose[2] = new Pose2d(new Translation2d(12.9, 7.2), new Rotation2d(Math.toRadians(15)));
+      blueStationPose[3] = new Pose2d(new Translation2d(13.8, 6.90), new Rotation2d(Math.toRadians(82)));
+
+      stationArmTargets[0] = new Translation2d(0.90, 1.05); //95
+      stationArmTargets[1] = new Translation2d(0.90, 1.05); //95
+      stationArmTargets[2] = new Translation2d(0.65, .2); //ground
+      stationArmTargets[3] = new Translation2d(0.51, .69); //.69
+
+
+
+
+
   }
   
 }
