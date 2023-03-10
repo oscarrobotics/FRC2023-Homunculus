6a851519fb612af3392a9195e1ef7cb9adb9d579
IanCharboneau <Ianchar13oneau@gmail.com>
2023-03-10 10:46:43 -0500

    nonworking auto

--- src/main/java/frc/robot/AutonomousSelector.java
+++ src/main/java/frc/robot/AutonomousSelector.java
@@ -4,15 +4,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import io.github.oblarg.oblog.annotations.Log;
+import java.util.ArrayList;
 import java.util.List;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 public class AutonomousSelector extends SubsystemBase{
 
-  SendableChooser<List <PathPlannerTrajectory>> m_chooser;
-
+  int m_autoSelected;
+  ArrayList<List <PathPlannerTrajectory>> m_chooser = new ArrayList<List <PathPlannerTrajectory>>();
+ArrayList<String> autoNames = new ArrayList<String>();
   public AutonomousSelector(){
     List<PathPlannerTrajectory> oneCargoAuto1 = 
         PathPlanner.loadPathGroup(
@@ -42,17 +44,41 @@ public class AutonomousSelector extends SubsystemBase{
         PathPlanner.loadPathGroup(
           "ThreeCargoAuto", 
           new PathConstraints(3, 3));
+    List<PathPlannerTrajectory> dropCone3AndLeaveComnityEndAtStation = 
+        PathPlanner.loadPathGroup(
+          "dropCone3AndLeaveComnityEndAtStation", 
+          new PathConstraints(3, 3));
+    List<PathPlannerTrajectory> dropCube3AndLeaveComnityEndAtStation = 
+        PathPlanner.loadPathGroup(
+          "dropCube3AndLeaveComnityEndAtStation", 
+          new PathConstraints(3, 3));
+      List<PathPlannerTrajectory> LeaveComnityEndAtStation = 
+        PathPlanner.loadPathGroup(
+          "LeaveComnityEndAtStation", 
+          new PathConstraints(3, 3));
     
-    m_chooser = new SendableChooser<>();
-    m_chooser.setDefaultOption("One Cargo Auto 1", oneCargoAuto1);
-    m_chooser.addOption("One Cargo Auto 2", oneCargoAuto2);
-    m_chooser.addOption("One Cargo Auto 3", oneCargoAuto3);
-    m_chooser.addOption("Two Cargo Auto 1", twoCargoAuto1);
-    m_chooser.addOption("Two Cargo Auto 2", twoCargoAuto2);
-    m_chooser.addOption("Two Cargo Auto 3", twoCargoAuto3);
-    m_chooser.addOption("Three Cargo Auto", threeCargoAuto); //NOT RECCOMENDED
-    
-    SmartDashboard.putData(m_chooser);
+
+    m_chooser.add(oneCargoAuto1);
+    m_chooser.add(oneCargoAuto2);
+    m_chooser.add(oneCargoAuto3);
+    m_chooser.add(twoCargoAuto1);
+    m_chooser.add(twoCargoAuto2);
+    m_chooser.add(twoCargoAuto3);
+    m_chooser.add(threeCargoAuto); //NOT RECCOMENDED
+    m_chooser.add(dropCone3AndLeaveComnityEndAtStation);
+    m_chooser.add(dropCube3AndLeaveComnityEndAtStation);
+   m_chooser.add(LeaveComnityEndAtStation);
+  
+   autoNames.add("One Cargo Auto 1");
+  autoNames.add("One Cargo Auto 2");
+  autoNames.add("One Cargo Auto 3");
+  autoNames.add("Two Cargo Auto 1");
+  autoNames.add("Two Cargo Auto 2");
+  autoNames.add("Two Cargo Auto 3");
+  autoNames.add("Three Cargo Auto");
+  autoNames.add("Drop Cone 3 and Leave Community End at Station");
+  autoNames.add("Drop Cube 3 and Leave Community End at Station");
+  autoNames.add("Leave Community End at Station");
 
   }
   
@@ -62,6 +88,7 @@ public class AutonomousSelector extends SubsystemBase{
     
   }
 
+  @Log(name = "autoselector", tabName = "Autonomous")
   public List<PathPlannerTrajectory> getSelectedAuto() {
     return m_chooser.getSelected();
   }
